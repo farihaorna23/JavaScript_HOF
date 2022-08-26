1909f46e2439cfb5fcd056b87f15ea4f62e5dfc5
farihaorna23 <farihaorna@gmail.com>
2022-08-26 11:50:48 -0400

    finished exexercise1

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+const plus = number => {
+  return function(plusNumber) {
+    return number + plusNumber;
+  };
+};
+
+const plus15 = plus(15);
+console.log(plus15(10));
