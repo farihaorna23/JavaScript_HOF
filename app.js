9a5d71570f5c14bf39e48ef2e578906f1c649658
farihaorna23 <farihaorna@gmail.com>
2022-08-26 11:59:15 -0400

    Finished exercise2

--- app.js
+++ app.js
@@ -8,5 +8,39 @@ const plus = number => {
   };
 };
 
-const plus15 = plus(15);
-console.log(plus15(10));
+// const plus15 = plus(15);
+// console.log(plus15(10));
+
+console.log("EXERCISE 1:\n==========\n");
+let users = [
+  {
+    name: "Frodo",
+    age: 50,
+    score: 85,
+    isActive: false
+  },
+  {
+    name: "Sam",
+    age: 38,
+    score: 94,
+    isActive: true
+  },
+  {
+    name: "Merry",
+    age: 36,
+    score: 82,
+    isActive: true
+  },
+  {
+    name: "Pippin",
+    age: 26,
+    score: 77,
+    isActive: false
+  }
+];
+
+const print = users => {
+  users.forEach(user => console.log(user.name));
+};
+
+print(users);
